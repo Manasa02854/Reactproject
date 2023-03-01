991a56fdf6fe9889007edeb9722b2e24cab161e9
Manasa02854 <75876211+Manasa02854@users.noreply.github.com>
2023-03-01 19:59:49 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 style={{color:"red"}}>Hello world</h1>
+   <h1>{1*5}</h1>
+   <p>ncjchducikacjceaca</p>
+    {/* <div className="App">
+       <header className="App-header">
+      
+      </header> 
+    </div> */}
+    </>
+  );
+
+}
+
+export default App;
